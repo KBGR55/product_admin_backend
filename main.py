96206a47b7eef104958416f6ca7365b4521a96a7
KBGR55 <brigithgonzaga501@gmail.com>
2026-01-20 12:36:07 -0500

    chore: adaptar servidor para puerto dinámico en despliegue

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 from waitress import serve
 from app import main as app_factory
 
 if __name__ == '__main__':
     app = app_factory({})
-    print("Server starting at http://localhost:6543")
-    serve(app, host='0.0.0.0', port=6543)
+
+    port = int(os.environ.get("PORT", 6543))
+    host = "0.0.0.0"
+
+    print(f"Server starting at http://{host}:{port}")
+    serve(app, host=host, port=port)
